a5b4b486cbb584b88d82a3ad6b0dbf974e915101
Paul Belanger <paul.belanger@polybeacon.com>
2012-10-10 13:09:55 -0400

    Add settings for passenger
    Signed-off-by: Paul Belanger <paul.belanger@polybeacon.com>

--- manifests/server.pp
+++ manifests/server.pp
@@ -3,6 +3,15 @@ class puppet::server(
   $monitor  = false,
   $options  = {},
 ) {
+  require common::client
+
+  $defaults = {
+    'ssl_client_header'         => 'SSL_CLIENT_S_DN',
+    'ssl_client_verify_header'  => 'SSL_CLIENT_VERIFY',
+  }
+
+  $options_real = merge($defaults, $options)
+
   include puppet::params::server
   include puppet::server::init
 }
